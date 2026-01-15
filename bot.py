e5a39d91e03c383a5783fddf8895f7e321f47e00
Afimin128899 <140860471+Afimin128899@users.noreply.github.com>
2026-01-15 12:24:00 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -4,40 +4,36 @@ import os
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
-from dotenv import load_dotenv
 from flyerapi import Flyer
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("8500994183:AAFuJAtatem_2olCueCceAPi9QxMOL08_EE")
-FLYER_API_KEY = os.getenv("FL-eliuMo-kzwWnO-uvimwU-UOfqjW")
 
 logging.basicConfig(level=logging.INFO)
 
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+FLYER_API_KEY = os.environ.get("FLYER_API_KEY")
+
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
-
 flyer = Flyer(FLYER_API_KEY)
 
 
 @dp.message(CommandStart())
-async def start_handler(message: types.Message):
+async def start(message: types.Message):
     await message.answer(
-        "Привет! Для использования бота нужно выполнить задания / подписаться."
+        "Привет! Для доступа нужно выполнить задания / подписаться."
     )
 
 
 @dp.message()
-async def check_flyer(message: types.Message):
+async def check(message: types.Message):
     ok = await flyer.check(
         user_id=message.from_user.id,
         language_code=message.from_user.language_code,
     )
 
     if not ok:
-        return  # Flyer сам отправит сообщение
+        return
 
-    await message.answer("✅ Проверка пройдена, доступ разрешён")
+    await message.answer("✅ Доступ разрешён")
 
 
 async def main():
